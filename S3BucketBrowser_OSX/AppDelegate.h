bb3886e3f24f3c6969a543b908b6d96c4f7d63b1
Matt Hayes <cruinh@gmail.com>
2014-05-26 17:37:04 -0500

    Initial Commit

---
+++ S3BucketBrowser_OSX/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  S3BucketBrowser_OSX
+//
+//  Created by cruinh on 5/26/14.
+//  Copyright (c) 2014 Matthew Hayes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ S3BucketBrowser_OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  S3BucketBrowser_OSX
+//
+//  Created by cruinh on 5/26/14.
+//  Copyright (c) 2014 Matthew Hayes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
